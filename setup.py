accb2ccb918481e375d28df67e69bdaacbf70fde
Jeffrey Gleason <jeffrey.gleason.3@gmail.com>
2019-01-27 13:45:07 -0600

    new d3m.primitives namespace specification

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(name='PcafeaturesD3MWrapper',
     ],
     entry_points = {
         'd3m.primitives': [
-            'distil.pcafeatures = PcafeaturesD3MWrapper:pcafeatures'
+            'distil.feature_selection.pcafeatures = PcafeaturesD3MWrapper:pcafeatures'
         ],
     },
 )
