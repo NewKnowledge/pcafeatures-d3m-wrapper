939faa2c726d3a093a571d87e3549647e066d440
Ubuntu <paul@newknowledge.io>
2019-02-18 01:25:13 +0000

    ading suggestedTarget

--- PcafeaturesD3MWrapper/wrapper.py
+++ PcafeaturesD3MWrapper/wrapper.py
@@ -115,6 +115,7 @@ class pcafeatures(TransformerPrimitiveBase[Inputs, Outputs, Hyperparams]):
         # drop all columns below some default threshold value
         # threshold is 0.0, i.e., any useful features should not be dropped
         bestFeatures = [int(row[1]) for row in pca_df.itertuples() if float(row[2]) > 0.01]
+        bestFeatures.append(int(30))
         from d3m.primitives.data_transformation.extract_columns import DataFrameCommon as ExtractColumns
         extract_client = ExtractColumns(hyperparams={"columns":bestFeatures})
         result=extract_client.produce(inputs=inputs)
