244d3fe59a1581bc2660c925109bb204875a8ef9
jgleason <jeffrey.gleason@newknowledge.io>
2019-04-12 15:13:28 -0500

    drop index

--- PcafeaturesD3MWrapper/wrapper.py
+++ PcafeaturesD3MWrapper/wrapper.py
@@ -120,7 +120,7 @@ class pcafeatures(TransformerPrimitiveBase[Inputs, Outputs, Hyperparams]):
         # remove primary key and targets from feature selection
         inputs_primary_key = inputs.metadata.get_columns_with_semantic_type('https://metadata.datadrivendiscovery.org/types/PrimaryKey')
         inputs_target = inputs.metadata.get_columns_with_semantic_type('https://metadata.datadrivendiscovery.org/types/SuggestedTarget')
-        inputs.drop(inputs_primary_key + inputs_target, axis=1, inplace=True)
+        inputs.drop(inputs_primary_key, axis=1, inplace=True)
 
         # extract numeric columns and suggested target
         if self.hyperparams['only_numeric_cols']:
@@ -128,7 +128,7 @@ class pcafeatures(TransformerPrimitiveBase[Inputs, Outputs, Hyperparams]):
             inputs_integer = inputs.metadata.get_columns_with_semantic_type('http://schema.org/Integer')
             inputs_numeric = [*inputs_float, *inputs_integer]
             inputs = inputs.iloc[:, inputs_numeric]
-            
+
         # generate feature ranking
         pca_df = PCAFeatures().rank_features(inputs = inputs)
 
